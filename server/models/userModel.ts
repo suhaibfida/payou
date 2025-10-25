b2815e341d70a4b668c1933b92afde688c4f1b01
suhaibfida <suhaibfida33@outlook.com>
2025-10-25 23:18:32 +0530

    define mongoose schema

---
+++ server/models/userModel.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  username: { type: String, require: true, unique: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+});
+const user = mongoose.model("user", userSchema);
+export default user;
