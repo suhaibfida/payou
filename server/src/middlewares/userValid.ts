f90c790654e4d40e7200f92ed517d71109d6f67d
suhaibfida <suhaibfida33@outlook.com>
2025-11-07 11:56:10 +0530

    middle ware

---
+++ server/src/middlewares/userValid.ts
@@ -0,0 +1,39 @@
+import type { Request, Response, NextFunction } from "express";
+import { Types } from "mongoose";
+import jwt from "jsonwebtoken";
+const key = process.env.SECRETKEY;
+export interface AuthRequest extends Request {
+  userID?: Types.ObjectId;
+}
+const middleware = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({
+        message: "Refresh the page",
+      });
+    }
+    if (!key) {
+      return res.status(401).json({
+        message: "Internal server error",
+      });
+    }
+
+    const decode = jwt.verify(token, key);
+    if (typeof decode !== "object" || !("userID" in decode)) {
+      throw new Error("Invalid token payload");
+    }
+    req.userID = decode.userID;
+
+    next();
+  } catch (error) {
+    console.log("error");
+    return res.status(500).json({
+      message: "server error",
+    });
+  }
+};
