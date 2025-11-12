623d0ff47b0971adefa7d0bb93fdc5fc955c86e3
suhaibfida <suhaibfida33@outlook.com>
2025-11-12 09:03:09 +0530

    add session transaction

--- server/src/controllers/account.ts
+++ server/src/controllers/account.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import type { AuthRequest } from "./../middlewares/userValid.js";
 import type { Response } from "express";
 import { User, Account } from "./../models/userModel.js";
@@ -16,35 +17,47 @@ export const balance = async (req: AuthRequest, res: Response) => {
   });
 };
 export const transfer = async (req: AuthRequest, res: Response) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
   const { amount, to } = req.body;
   const account = await Account.findOne({
     userid: req.userID,
-  });
+  }).session(session);
   if (!account) {
+    await session.abortTransaction();
+    session.endSession();
     return res.json({ message: "Please create account first" });
   }
   if (!amount || !to) {
+    await session.abortTransaction();
+    session.endSession();
     return res.status(400).json({
       message: "Please enter the amount to transfer and the recepient account",
     });
   }
   const check = await User.findOne({
     username: to,
-  });
+  }).session(session);
   if (!check) {
+    await session.abortTransaction();
+    session.endSession();
     return res.json({
       message: "Please enter valid name",
     });
   }
-  if (!account.balance < amount) {
+  if (account.balance < amount) {
+    await session.abortTransaction();
+    session.endSession();
     return res.json({
       message: "Balance is too low",
     });
   }
   const checkRecepient = await Account.findOne({
     userid: check._id,
-  });
+  }).session(session);
   if (!checkRecepient) {
+    session.abortTransaction();
+    session.endSession();
     return res.json({
       message: "User Account does not exist",
     });
@@ -58,17 +71,18 @@ export const transfer = async (req: AuthRequest, res: Response) => {
         balance: -amount,
       },
     }
-  );
-  await Account.updateOne(
+  ).session(session);
+  const recep = await Account.updateOne(
     {
       userid: check._id,
     },
     {
       $inc: {
-        balance: -amount,
+        balance: amount,
       },
     }
-  );
+  ).session(session);
+  await session.commitTransaction();
   res.json({
     message: "Amount transferred successfully, your balance is",
     balance: account.balance,
